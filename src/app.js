3ad5e986e2e30f603038f3d4517964e63c6360d2
TonyUCLA <TonyUCLA>
2017-03-13 15:40:31 -0700

    map and places imported

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import Map from './components/Map'
+import Places from './components/Places'
 
 class App extends Component {
     render() {
         return (
             <div>
                 This is the React App
+                    <Map />
             </div>
         )
     }
