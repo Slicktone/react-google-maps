4cfac63777dc42f27835808236898d637dfa8c15
TonyUCLA <TonyUCLA>
2017-03-13 19:29:37 -0700

    react-google-maps updated

--- src/app.js
+++ src/app.js
@@ -4,11 +4,62 @@ import Map from './components/Map'
 import Places from './components/Places'
 
 class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            venues: []
+        }
+    }
+
+
+
+    componentDidMount() {
+        console.log('componentDidMount')
+
+        // make api call here to eventful API using Axios or SuperAgent
+        const url= // API url goes here
+
+        superagent
+        .get(url)
+        .query(null)
+        .set('Accept', 'text/json')
+        .end((error, response) => {
+
+            const venues = response.body.response.venues
+            console.log(JSON.stringify(venues))
+            this.setState({
+                venues: venues
+            })
+
+        })
+        
+    }
     render() {
+        const location = {
+        lat: 40.7575285,
+        lng: -73.9884469
+    }
+
+    const markers = [
+        {
+            location:{
+                lat: 40.7575285,
+                lng: -73.9884469
+            }
+        }
+    ]
+// once the api is set for the markers use {this.state.venues} while replacing the venues
         return (
             <div>
                 This is the React App
-                    <Map />
+                <div style={{width:300, height:600, background:'red'}}>
+                    <Map center={location} markers={markers} />
+                    
+
+                </div>
+
+
+                    <Places  />
             </div>
         )
     }
--- src/components/Places.js
+++ src/components/Places.js
@@ -2,8 +2,20 @@ import React, { Component } from 'react'
 
 class Places extends Component {
     render() {
+        const list = this.props.venues.map((venue, i) => {
+            return (
+                // check api response to check key/value
+                <li key={i}>{venue.name}</li>
+            )
+
+        })
+
         return (
-            <div> This is the Places Component </div>
+            <div> 
+                <ol>
+                    {list}
+                </ol>
+            </div>
         )
     }
 }
